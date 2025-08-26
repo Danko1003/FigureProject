e84962c153773ef382eb84d6d219e0253039f81d
Daniel <koivunendan10@gmail.com>
2025-08-26 13:52:52 -0400

    Circle

---
+++ Circle.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Circle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Circle
+{
+    // instance variables - replace the example below with your own
+    private int xpos;
+    private int ypos;
+    private int width;
+    private int height;
+
+    /**
+     * Constructor for objects of class Circle
+     */
+    public Circle()
+    {
+        // initialise instance variables
+        xpos = 0;
+        ypos = 0;
+        width = 100;
+        height = 100;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int render(int xpos, int ypos, int width, int height)
+    {
+        // put your code here
+        return xpos;
+    }
+}
